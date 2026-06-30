f1b744309052dfd9bc33a54c3ad553771a257be0
Igor Lima Dos Santos <igorlimasantos667@gmail.com>
2026-06-30 10:03:47 -0300

    exercicio14

--- src/exercicios/Exercicio14ControleDeEmprestimos.java
+++ src/exercicios/Exercicio14ControleDeEmprestimos.java
@@ -1,26 +1,49 @@
 package exercicios;
-public static void main(String[] args){
-    int emprestimoLiberados = 0;
-    int emprestimosNegados = 0;
+public class Exercicio14ControleDeEmprestimos {
+    public static void main(String[] args) {
+        int emprestimoLiberados = 0;
+        int emprestimosNegados = 0;
 
-    for(int livro = 1; livro <= 4; livro++){
-        switch (livro){
-            case 1:
-            System.out.println("Livro" + livro + "Java Basico");
-            break;
-            case 2:
-                System.out.println("Livro "+ livro + "Algoritmos");
-                break;
-            case 3:
-                System.out.println("Livro " + livro + " Banco de dados");
-                break;
-            case 4:
-                System.out.println("Livro" + livro + "Redes de Computadores");
-                break;
-            default:
-                System.out.println("Livro infantil");
+        for (int livro = 1; livro <= 4; livro++) {
+            switch (livro) {
+                case 1:
+                    System.out.println(" Livro " + livro + " Java Basico ");
+                    break;
+                case 2:
+                    System.out.println(" Livro " + livro + " Algoritmos ");
+                    break;
+                case 3:
+                    System.out.println(" Livro " + livro + " Banco de dados ");
+                    break;
+                case 4:
+                    System.out.println(" Livro " + livro + " Redes de Computadores ");
+                    break;
+                default:
+                    System.out.println("Livro infantil");
+            }
+            boolean usuarioAtivo = livro != 2;
+            boolean livroDisponivel = livro != 3;
+            boolean multaPendente = livro == 4;
+
+            boolean emprestimoLiberado = usuarioAtivo && livroDisponivel && !multaPendente;
+
+            if (emprestimoLiberado) {
+                System.out.println("Emprestimo Liberado");
+                emprestimoLiberados++;
+            }else if(multaPendente){
+                System.out.println("Emprestimo negado : usuario possui multa pendente");
+                emprestimosNegados++;
+            }else if(!usuarioAtivo){
+                System.out.println("Emprestimo negado : usuario inativo");
+                emprestimosNegados++;
+            }else {
+                System.out.println("Emprestimo negado : Livro indisponivel");
+                emprestimosNegados++;
+            }
+            System.out.println();
         }
-        boolean usuarioAtivo = livro != 2;
-        boolean livroDisponivel = livro !=3;
+        System.out.println("Resumo");
+        System.out.println("Emprestimo liberados"  + emprestimoLiberados);
+        System.out.println("Emprstimo Negados :" + emprestimosNegados );
     }
 }
